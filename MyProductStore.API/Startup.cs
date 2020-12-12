41a140e88a00bea1ddaa1617286898d08580d1be
javierreyes10 <javier.reyes10@outlook.com>
2020-12-12 12:24:49 -0600

    Added Database configuration over Startup class

--- MyProductStore.API/Startup.cs
+++ MyProductStore.API/Startup.cs
@@ -1,15 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MyProductStore.Core.Interfaces;
+using MyProductStore.Infrastructure.Data;
+using MyProductStore.Infrastructure.Repositories;
 
 namespace MyProductStore.API
 {
@@ -26,6 +23,11 @@ namespace MyProductStore.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ProductStoreDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default"),
+                x => x.MigrationsAssembly("MyProductStore.Infrastructure")), ServiceLifetime.Transient);
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
