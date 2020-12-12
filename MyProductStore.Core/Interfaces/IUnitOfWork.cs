37bc7597ce693d86293c37670e9821e3653d3060
javierreyes10 <javier.reyes10@outlook.com>
2020-12-12 10:58:41 -0600

    Added IRepository and IUnitOfWork interfaces

---
+++ MyProductStore.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using MyProductStore.Core.Entities;
+using MyProductStore.Core.Repositories;
+using System.Threading.Tasks;
+
+namespace MyProductStore.Core.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Product> Products { get; }
+        Task<int> CommitAsync();
+    }
+}
