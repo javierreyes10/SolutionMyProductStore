d3d92050835248696dedeb4d4b6f461b5fea734d
javierreyes10 <javier.reyes10@outlook.com>
2020-12-12 12:40:46 -0600

    DB Creation Migration

---
+++ MyProductStore.Infrastructure/Migrations/20201212182832_CreateDB.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyProductStore.Infrastructure.Migrations
+{
+    public partial class CreateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Producto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SKU = table.Column<string>(unicode: false, maxLength: 20, nullable: true),
+                    Nombre = table.Column<string>(maxLength: 100, nullable: false),
+                    Cantidad = table.Column<short>(nullable: false),
+                    Precio = table.Column<decimal>(nullable: false),
+                    Descripcion = table.Column<string>(maxLength: 250, nullable: true),
+                    Imagen = table.Column<string>(unicode: false, maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producto", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Producto");
+        }
+    }
+}
