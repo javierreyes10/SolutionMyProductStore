cebe255b07b3d588caf9de746736519665f893c4
javierreyes10 <javier.reyes10@outlook.com>
2020-12-12 11:55:04 -0600

    Added UnitOfWork and Repository implementation

---
+++ MyProductStore.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyProductStore.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MyProductStore.Infrastructure.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await Context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await Context.Set<TEntity>().ToListAsync();
+        }
+
+        public async ValueTask<TEntity> GetByIdAsync(int id)
+        {
+            return await Context.Set<TEntity>().FindAsync(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+
+        public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await Context.Set<TEntity>().SingleOrDefaultAsync(predicate);
+        }
+    }
+}
---
+++ MyProductStore.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using MyProductStore.Core.Entities;
+using MyProductStore.Core.Interfaces;
+using MyProductStore.Core.Repositories;
+using MyProductStore.Infrastructure.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace MyProductStore.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly ProductStoreDbContext _context;
+        private IRepository<Product> _productRepository;
+
+        public UnitOfWork(ProductStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<Product> Products => _productRepository ?? new Repository<Product>(_context);
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
