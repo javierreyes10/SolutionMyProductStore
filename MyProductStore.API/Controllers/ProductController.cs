1706b104f1579da2798fee1b052b1d21b358d38d
javierreyes10 <javier.reyes10@outlook.com>
2020-12-12 14:05:56 -0600

    Added ProductDto

---
+++ MyProductStore.API/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProductStore.Core.Entities;
+using MyProductStore.Core.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyProductStore.API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts()
+        {
+            return Ok(await _unitOfWork.Products.GetAllAsync());
+        }
+    }
+}
---
+++ MyProductStore.Application/DTOs/Output/ProductOutputDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MyProductStore.Application.DTOs.Output
+{
+    public class ProductOutputDto
+    {
+        public int Id { get; set; }
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public short Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
