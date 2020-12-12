24fa44c0603b37026a33370a960025394048b0fc
javierreyes10 <javier.reyes10@outlook.com>
2020-12-12 11:34:25 -0600

    Added EF Core, ProductConfiguration and DBContext

---
+++ MyProductStore.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyProductStore.Core.Entities;
+
+namespace MyProductStore.Infrastructure.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder
+                .HasKey(p => p.Id);
+
+            builder
+                .Property(p => p.Id)
+                .UseIdentityColumn();
+
+            builder
+                .Property(p => p.Name)
+                .HasColumnName("Nombre")
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder
+                .Property(p => p.Quantity)
+                .HasColumnName("Cantidad")
+                .IsRequired();
+
+            builder
+               .Property(p => p.Price)
+               .HasColumnName("Precio")
+               .IsRequired();
+
+            builder
+                .Property(p => p.Description)
+                .HasColumnName("Descripcion")
+                .HasMaxLength(250);
+
+            builder
+                .Property(p => p.Image)
+                .HasColumnName("Imagen")
+                .HasMaxLength(500)
+                .IsUnicode(false);
+
+            builder
+                .Property(p => p.SKU)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        }
+    }
+}
---
+++ MyProductStore.Infrastructure/Data/ProductStoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyProductStore.Core.Entities;
+using System.Reflection;
+
+namespace MyProductStore.Infrastructure.Data
+{
+    public class ProductStoreDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductStoreDbContext(DbContextOptions<ProductStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
