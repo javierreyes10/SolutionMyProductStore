df9e606cdff358e8196a60a0a46b3f673181afc1
javierreyes10 <javier.reyes10@outlook.com>
2020-12-16 00:38:22 -0600

    added the UserName on UserOutputDto

--- MyProductStore.Application/DTOs/Output/User/UserOutputDto.cs
+++ MyProductStore.Application/DTOs/Output/User/UserOutputDto.cs
@@ -6,6 +6,7 @@ namespace MyProductStore.Application.DTOs.Output.User
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string UserName { get; set; }
         public string Phone { get; set; }
         public DateTime BirthdDate { get; set; }
 
