7efed492dcc6626932c7adaf9884704148bb1a8a
javierreyes10 <javier.reyes10@outlook.com>
2020-12-16 00:47:55 -0600

    SQL script updated

--- myProductStore.sql
+++ myProductStore.sql
@@ -232,10 +232,6 @@ VALUES (N'20201215221131_AddingUsuario', N'3.1.1');
 
 GO
 
-INSERT INTO[dbo].[Usuario] ([Nombre], [Telefono], [Usuario], [FechaNacimiento], [Email], [PasswordHash], [ResetToken], [ResetTokenExpires]) VALUES(N'John Dao', N'77978541', N'Dao.John', N'1980-05-05 00:00:00', N'john.dao@test.com', N'$2a$11$FS1QcNLDbTbXy9W6P4xZh.Z6zGFZsRLncM97SskdV2FfASKSl0pQi', NULL, NULL)
-
-GO
-
 INSERT INTO[dbo].[Usuario] ([Nombre], [Telefono], [Usuario], [FechaNacimiento], [Email], [PasswordHash], [ResetToken], [ResetTokenExpires]) VALUES(N'Michael Jackson', N'77978541', N'Jackson.Michael', N'1980-05-05 00:00:00', N'michael.jackson@test.com', N'$2a$11$FS1QcNLDbTbXy9W6P4xZh.Z6zGFZsRLncM97SskdV2FfASKSl0pQi', NULL, NULL)
 
 GO
